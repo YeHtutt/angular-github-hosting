2fe008bd29896b1fb6a51fcaf1f9f1c5b8762f37
YeHtutAung <yehaung.nov@gmail.com>
2023-06-26 14:26:03 +0200

    changed header text and animated it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'angular-github-hosting-project';
   images = ['demo1.jpg', 'demo2.jpg', 'demo3.jpg'];
+  headlines = ['Bring engineering to the next level',
+    'Fun with programming',
+    'graduated in HDA and in Developer Akademie'];
+
   currentImage = 0;
   showImage = true;
 
 
   /**This function implements the components */
-  ngOnInit(){ 
+  ngOnInit() {
     this.updateImage();
   }
 
 
 
-  updateImage(){
+  updateImage() {
     /**every 8 Sec. increment the index */
-    setInterval(()=> {
+    setInterval(() => {
       this.currentImage++;
       this.currentImage = this.currentImage % this.images.length; //every 8Sec. count and change 0..1..2 
       this.showImage = false;
@@ -31,7 +35,7 @@ export class AppComponent implements OnInit {
       }, 100);
 
     }, 8000)
-    
+
   }
 
 }
