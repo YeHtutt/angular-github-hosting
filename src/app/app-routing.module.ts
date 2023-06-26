635eb6d598cd3385ccf84c275f3d4903864ec67e
YeHtutAung <yehaung.nov@gmail.com>
2023-06-26 15:57:43 +0200

    started with the first route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SlideshowComponent }, //wenn bei route gar nicht ist Bilder-Slideshow(Home) angezeigt werden
+  { path: 'imprint', component: ImprintComponent } //wenn route /imprint hinten ist, dann soll zur Impressum-seite umgeleitet werden
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
